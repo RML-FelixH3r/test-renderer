19f8863eeb7bc62b1427e170e8bfc766df638db5
Felix Hermanutz <felix.hermanutz@MBP-von-Felix-2.fritz.box>
2023-08-22 15:08:33 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+  import RoomleComponent from '/src/components/RoomleComponent.vue';
+  import {ref} from "vue";
+
+  const configurationIds = ref(['usm:frame', 'allnatura:showcase_2', 'jab:soulmate', 'visplay:qubo_preset_VAR5']);
+</script>
+
+<template>
+  <!--  <div class="input-wrapper">-->
+  <!--    <input ref="mapInput" placeholder="environment map"/>-->
+  <!--    <input ref="intensityInput" placeholder="map intensity"/>-->
+  <!--  </div>-->
+  <div class="roomle-wrapper">
+    <div class="roomle-col">
+      <roomle-component v-for="configurationId in configurationIds"
+                        :configuration-id="configurationId"
+      />
+    </div>
+    <div class="roomle-col">
+      <roomle-component v-for="configurationId in configurationIds"
+                        :configuration-id="configurationId"
+                        override-server-url="https://alpha.roomle.com/t/new-renderer/"
+      />
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+  .roomle-wrapper {
+    display: flex;
+    gap: 2rem;
+    justify-content: center;
+  }
+
+  .roomle-col {
+    display: flex;
+    width: 45vw;
+    gap: 2rem;
+    flex-direction: column;
+  }
+
+  .input-wrapper {
+    margin-bottom: 2rem;
+  }
+</style>
---
+++ src/components/RoomleComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div ref="roomleContainer" class="roomle-container"></div>
+</template>
+
+<script lang="ts" setup>
+  import {onMounted, ref} from "vue";
+  import RoomleConfiguratorApi from '@roomle/embedding-lib/roomle-configurator-api';
+
+  const props = defineProps({
+        overrideServerUrl: String,
+        configurationId: String,
+        environmentMapUrl: String,
+        environmentMapIntensity: Number
+      }
+  );
+  const roomleContainer = ref(null);
+  onMounted(async () => {
+    const options = {
+      overrideServerUrl: props.overrideServerUrl,
+      id: props.configurationId,
+      envMapUrl: props.environmentMapUrl,
+      envMapIntensity: props.environmentMapIntensity
+    };
+    if (!roomleContainer.value) {
+      console.log("null");
+    }
+    const configurator = await RoomleConfiguratorApi.createConfigurator(
+        "demoConfigurator",
+        roomleContainer.value,
+        options
+    );
+    if (configurator.ui) {
+      console.log("init");
+    }
+  });
+</script>
+
+<style scoped>
+  .roomle-container {
+    width: 100%;
+    height: 50vh;
+  }
+</style>
