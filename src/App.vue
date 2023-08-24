1ba2fc8644df3b6f6e9ddf033964b795d32757db
FelixH3r <felix.hermanutz@gmx.at>
2023-08-24 14:23:10 +0200

    add input fields

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
-<script lang="ts" setup>
-  import RoomleComponent from '@/components/RoomleComponent.vue';
-  import {ref} from "vue";
-
-  const configurationIds = ref(['usm:frame', 'allnatura:showcase_2', 'jab:soulmate', 'visplay:qubo_preset_VAR5']);
-</script>
-
 <template>
-  <!--  <div class="input-wrapper">-->
-  <!--    <input ref="mapInput" placeholder="environment map"/>-->
-  <!--    <input ref="intensityInput" placeholder="map intensity"/>-->
-  <!--  </div>-->
+  <div class="input-wrapper">
+    <label>environment map (search on: <a href="https://polyhaven.com/hdris" target="_blank">polyhaven</a>): </label>
+    <input v-model="mapInput" placeholder="environment map"/>
+    <label>environment map intensity (also decimal allowed): </label>
+    <input v-model="intensityInput" placeholder="map intensity" type="number"/>
+    <label>configurationIds (divided by commas): </label>
+    <textarea v-model="configurationIdsInput"
+              placeholder="usm:frame, allnatura:showcase_2, jab:soulmate, visplay:qubo_preset_VAR5"/>
+    <button @click="applyValues">Test</button>
+
+  </div>
   <div class="roomle-wrapper">
-    <div class="roomle-col">
+    <div v-if="showOld" class="roomle-col">
       <roomle-component v-for="configurationId in configurationIds"
                         :configuration-id="configurationId"
       />
     </div>
-    <div class="roomle-col">
+    <div v-if="showNew" class="roomle-col">
       <roomle-component v-for="configurationId in configurationIds"
                         :configuration-id="configurationId"
-                        override-server-url="https://alpha.roomle.com/t/new-renderer/"
+                        :environment-map-intensity="intensityInput"
+                        :environment-map-url="mapInput"
+                        :override-server-url="'https://alpha.roomle.com/t/new-renderer/'"
       />
     </div>
   </div>
-
 </template>
+
+<script lang="ts" setup>
+  import RoomleComponent from '@/components/RoomleComponent.vue';
+  import {computed, nextTick, Ref, ref} from "vue";
+
+  const mapInput = ref(undefined);
+  const intensityInput = ref(2);
+  const configurationIdsInput: Ref<string | undefined> = ref(undefined);
+  let configurationIds = ['usm:frame', 'allnatura:showcase_2', 'jab:soulmate', 'visplay:qubo_preset_VAR5'];
+  const showNew = ref(true);
+  const showOld = ref(true);
+
+
+  computed(() => console.log(intensityInput.value));
+
+  const applyValues = () => {
+    if (!showNew.value || !showOld.value) {
+      return;
+    }
+    showNew.value = false;
+    if (configurationIdsInput.value) {
+      const str = configurationIdsInput.value?.replace(/ /g, '');
+      configurationIds = str.split(",");
+      showOld.value = false;
+      nextTick(() => showOld.value = true);
+    }
+    nextTick(() => showNew.value = true);
+  };
+
+  console.log(intensityInput);
+
+</script>
 
 <style scoped>
   .roomle-wrapper {
@@ -41,6 +74,13 @@
   }
 
   .input-wrapper {
+    display: flex;
+    flex-direction: column;
     margin-bottom: 2rem;
+    width: 30%;
+  }
+
+  input, textarea {
+    margin-bottom: 0.5rem;
   }
 </style>
--- src/components/RoomleComponent.vue
+++ src/components/RoomleComponent.vue
@@ -3,37 +3,39 @@
 </template>
 
 <script lang="ts" setup>
-  import {onMounted, ref} from "vue";
+  import type {PropType} from "vue";
+  import {onMounted, reactive, ref} from "vue";
   // @ts-ignore
   import RoomleConfiguratorApi from '@roomle/embedding-lib/roomle-configurator-api';
 
   const props = defineProps({
-        overrideServerUrl: String,
-        configurationId: String,
-        environmentMapUrl: String,
-        environmentMapIntensity: Number
+        overrideServerUrl: String as PropType<string | null>,
+        configurationId: String as PropType<string | null>,
+        environmentMapUrl: String as PropType<string | null>,
+        environmentMapIntensity: Number as PropType<number | null>
       }
   );
   const roomleContainer = ref(null);
+  const options = reactive({
+    overrideServerUrl: props.overrideServerUrl,
+    id: props.configurationId,
+    envMapUrl: props.environmentMapUrl,
+    envMapIntensity: props.environmentMapIntensity
+  });
   onMounted(async () => {
-    const options = {
-      overrideServerUrl: props.overrideServerUrl,
-      id: props.configurationId,
-      envMapUrl: props.environmentMapUrl,
-      envMapIntensity: props.environmentMapIntensity
-    };
-    if (!roomleContainer.value) {
-      console.log("null");
-    }
-    const configurator = await RoomleConfiguratorApi.createConfigurator(
+    await loadConfigurator();
+    // @ts-ignore
+    console.log(roomleContainer.value);
+  });
+
+  const loadConfigurator = async () => {
+    await RoomleConfiguratorApi.createConfigurator(
         "demoConfigurator",
         roomleContainer.value,
         options
     );
-    if (configurator.ui) {
-      console.log("init");
-    }
-  });
+  };
+
 </script>
 
 <style scoped>
